42c343a62cebd5cc3ac171716074c3470813d1a4
SigCatta <luca.cattani@mail.polimi.it>
2023-12-22 17:59:23 +0100

    added mail server project

---
+++ code/MailService/src/test/java/ckb/MailService/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ckb.MailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
