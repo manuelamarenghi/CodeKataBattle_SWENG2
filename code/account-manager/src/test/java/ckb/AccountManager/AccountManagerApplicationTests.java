885a2683cba869ac8b688c58ee04d7e529f19288
SigCatta <luca.cattani@mail.polimi.it>
2023-12-27 12:55:44 +0100

    added account-manager template

---
+++ code/account-manager/src/test/java/ckb/AccountManager/AccountManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ckb.AccountManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
