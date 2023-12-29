dc2e581efb676c39ab4a2c73eae792b554734761
manuelamarenghi <manuela.marenghi@mail.polimi.it>
2023-12-29 13:44:39 +0100

    comm

--- code/TournamentManager/src/main/java/ckb/TournamentManager/repo/TournamentRepo.java
+++ code/TournamentManager/src/main/java/ckb/TournamentManager/repo/TournamentRepo.java
@@ -3,8 +3,13 @@ package ckb.TournamentManager.repo;
 import ckb.TournamentManager.model.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface TournamentRepo extends JpaRepository<Tournament,Long> {
+
+    @Override
+    Optional<Tournament> findById(Long aLong);
 
 
 }
