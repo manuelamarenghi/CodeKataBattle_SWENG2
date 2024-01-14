c8b9e7d5714b848ff05a305fa821d58728188fbe
tommi <tommi.felle@gmail.com>
2024-01-14 20:56:27 +0100

    Added some tests in InviteStudentToTeamControllerTest and RegisterStudentToTeamControllerTest and added the feature CanCloseTournamentController with its test

--- code/battle-manager/src/main/java/ckb/BattleManager/model/Battle.java
+++ code/battle-manager/src/main/java/ckb/BattleManager/model/Battle.java
@@ -32,4 +32,6 @@ public class Battle {
 
     private Boolean battleToEval;
 
+    private Long authorId;
+
 }
--- code/battle-manager/src/test/java/ckb/BattleManager/InviteStudentToTeamControllerTest.java
+++ code/battle-manager/src/test/java/ckb/BattleManager/InviteStudentToTeamControllerTest.java
@@ -1,17 +1,46 @@
 package ckb.BattleManager;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import ckb.BattleManager.controller.InviteStudentToTeamController;
+import ckb.BattleManager.dto.input.StudentTeam;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class InviteStudentToTeamControllerTest {
+    private final InviteStudentToTeamController inviteStudentToTeamController;
+    private final ClientAndServer mockServer;
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    public InviteStudentToTeamControllerTest(InviteStudentToTeamController inviteStudentToTeamController) {
+        this.inviteStudentToTeamController = inviteStudentToTeamController;
+        mockServer = ClientAndServer.startClientAndServer(8085);
     }
 
-    @AfterEach
+    @Test
+    public void inviteStudentToTeam() {
+        mockServer
+                .when(HttpRequest.request().withMethod("POST").withPath("/api/mail/direct"))
+                .respond(HttpResponse.response().withStatusCode(200));
+
+        ResponseEntity<Object> response = inviteStudentToTeamController.inviteStudentToTeam(new StudentTeam(
+                1L, 1L
+        ));
+
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+    }
+
+    @AfterAll
     void tearDown() {
+        mockServer.stop();
     }
 }
